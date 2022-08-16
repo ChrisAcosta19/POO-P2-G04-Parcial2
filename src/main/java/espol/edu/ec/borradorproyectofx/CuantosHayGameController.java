f7bb27e47b551329ceaef3a4fe2b42f92637ca00
jppluas <jppluas@espol.edu.ec>
2022-08-15 22:20:54 -0500

    Actualización Juego
    Implementado el código para mostrar cada ejercicio yy verifica que la respuesta sea correcta

--- src/main/java/espol/edu/ec/borradorproyectofx/CuantosHayGameController.java
+++ src/main/java/espol/edu/ec/borradorproyectofx/CuantosHayGameController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
+
 package espol.edu.ec.borradorproyectofx;
 
 import java.io.FileInputStream;
@@ -13,10 +10,12 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 
 import javafx.scene.image.ImageView;
+import javafx.scene.input.InputMethodEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 /**
@@ -25,7 +24,6 @@ import javafx.scene.layout.GridPane;
  * @author Usuario
  */
 public class CuantosHayGameController implements Initializable {
-
 
     @FXML
     private BorderPane mainPane;
@@ -55,38 +53,105 @@ public class CuantosHayGameController implements Initializable {
     private ImageView btnRetroceder;
     @FXML
     private ImageView respuestaVisual;
+    @FXML
+    private Button btnVerificarRespuesta;
     
     String[] images={"cow","cowg","cowh","duck","horse","horsea","horseb","pig","pigb","rooster","roosterb","sheep"};
     ImageView[] imagesLocation={img01,img02,img11,img12,img03,img13,img00,img10};
     private ArrayList <Integer> numImagenesXEjercicio=new ArrayList<>();
+    private Boolean[] ToF={true,false};
+    private int ejercicio=0;
+    ArrayList<Ejercicio> ejercicios=new ArrayList<>();
     
     
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         setImage("arrow_right",btnAvanzar);
         setImage("arrow_left",btnRetroceder);
         
         btnRetroceder.setOnMouseClicked(eh -> {
+            if(ejercicio==0){
             try {
                 App.setRoot("cuantosHayMain");
             } catch (IOException ex) {
                 ex.printStackTrace();
-            }
+            }} else{ejercicio--;}
         });
         //asumiendo que este es el numero ingresado en el text field del main controller
-        ArrayList <ImagenesEjercicio> imagenesGame= new ArrayList<>();
-        Game g1=new Game(20);
+        ArrayList <Ejercicio> ejerciciosVacio= new ArrayList<>();
+        
+        Game g1=new Game(20,ejerciciosVacio);
         numImagenesXEjercicio=g1.imagesPerQuestion();
         for(int x:numImagenesXEjercicio){
-            ImagenesEjercicio l1=new ImagenesEjercicio(g1.imagesSelection(x,true));
-            imagenesGame.add(l1);
+            ArrayList <String> imagenesModelo= new ArrayList <>();
+            int j=(int) Math.floor(Math.random()*2); boolean bool=false;
+            if (j==1){bool=true;}
+            g1.imagesSelection(x,bool,imagenesModelo);
+            Ejercicio lista=new Ejercicio(x,imagenesModelo,0);
+            g1.ejercicios.add(lista); 
+            
         }
-
-                
+        System.out.println("AAAAAAAAAAAAAAAAAA");
+        
+        for(Ejercicio ej:g1.ejercicios){
+            for(String s:ej.imagenes){
+                System.out.println(s);
+            }
+            System.out.println("--------------------");
+        }
+        
+        
+        try{
+        ejercicio(g1,ejercicio);
+        btnVerificarRespuesta.setOnMouseClicked(eh -> {
+            if(Integer.valueOf(fieldRespuesta.getText())==g1.ejercicios.get(ejercicio).respuesta){
+            setImage("happy",respuestaVisual);
+            } else{setGif("globoe",respuestaVisual);}
+            
+            });
+        }catch (Exception ex) {
+                System.out.println("AQUI ES");
+                ex.printStackTrace();}
+        
+        btnAvanzar.setOnMouseClicked(eh -> {
+            respuestaVisual.imageProperty().set(null);
+            fieldRespuesta.clear();
+            System.out.println("--------------------------");
+            ejercicio++;
+            try {
+                ejercicio(g1,ejercicio);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (IndexOutOfBoundsException ex){
+                System.out.println("SE ACABO EL PROGRAMA");
+            }
+        });
+        
+        btnRetroceder.setOnMouseClicked(eh -> {
+            respuestaVisual.imageProperty().set(null);
+            fieldRespuesta.clear();
+            System.out.println("--------------------------");
+            ejercicio--;
+            try {
+                ejercicio(g1,ejercicio);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (IndexOutOfBoundsException ex){
+                System.out.println("SE ACABO EL PROGRAMA");
+            }
+        });
+        
         
     }
-
+    
+    void ejercicio(Game g, int ejercicio)throws IOException {
+        img00.imageProperty().set(null);img01.imageProperty().set(null);img02.imageProperty().set(null);
+        img03.imageProperty().set(null);img10.imageProperty().set(null);img11.imageProperty().set(null);
+        img12.imageProperty().set(null);img13.imageProperty().set(null);
+        ArrayList<String> imgs=new ArrayList<>();
+        g.imagesLocation(g.ejercicios.get(ejercicio).imagenes);
+    }
+    
     void setImage(String name,ImageView iView){
         InputStream input = null;
            Image image = null;
@@ -101,16 +166,41 @@ public class CuantosHayGameController implements Initializable {
                 if (input != null) {
                     try {
                         input.close();
-                    } catch (IOException ex) {
+                    } catch (Exception ex) {
                         System.out.println("Error al cerrar el recurso");
                     }
                 }
             }
     }
     
+    void setGif(String name,ImageView iView){
+        InputStream input = null;
+           Image image = null;
+            try {
+                input = new FileInputStream(App.pathImgGame + name +".gif");
+                image = new Image(input, 100, 100, false, false);
+                iView.setImage(image);
+
+            } catch (Exception ex) {
+                System.out.println("No se pudo cargar imagen");
+            } finally {
+                if (input != null) {
+                    try {
+                        input.close();
+                    } catch (Exception ex) {
+                        System.out.println("Error al cerrar el recurso");
+                    }
+                }
+            }
+    }
+
+    
+    
+    
     class Game {
 
     private int numEjercicios;
+    private ArrayList<Ejercicio> ejercicios;
     private ArrayList<Integer> respuestasCorrectas;
     private ArrayList<Integer> respuestasObtenidas;
     private ArrayList<Integer> numIntentosxEjercicio; //intentos hasta acertar
@@ -118,13 +208,13 @@ public class CuantosHayGameController implements Initializable {
     public Game(int numEjercicios){
         this.numEjercicios = numEjercicios;
     }
-    
-    public Game(int numEjercicios, ArrayList<Integer> numImagenesxEjercicio, ArrayList<String> imagenesxEjercicio, ArrayList<Integer> numIntentosxEjercicio) {
+
+    public Game(int numEjercicios, ArrayList<Ejercicio> ejercicios) {
         this.numEjercicios = numEjercicios;
-        this.numImagenesxEjercicio = numImagenesxEjercicio;
-        this.imagenesxEjercicio = imagenesxEjercicio;
-        this.numIntentosxEjercicio = numIntentosxEjercicio;
+        this.ejercicios = ejercicios;
     }
+
+    
     
     ArrayList <Integer> imagesPerQuestion(){
     ArrayList <Integer> imagesPerQ=new ArrayList<>();
@@ -173,8 +263,7 @@ public class CuantosHayGameController implements Initializable {
     
     
     
-    ArrayList<String> imagesSelection(int n, boolean imgDif){
-        ArrayList<String> imagenesElegidas=new ArrayList<>();
+    void imagesSelection(int n, boolean imgDif,ArrayList<String> imagenesElegidas){
         String imagenElegida=null;
         ImageView iv=null;
         if (imgDif){
@@ -188,19 +277,18 @@ public class CuantosHayGameController implements Initializable {
                 imagenesElegidas.add(imagenElegida);
             }
         }
-        return imagenesElegidas;
     }
     
     ImageView getIView(int i){
         ImageView iv=null;
-        if(i==0){iv=img01;}
-        if(i==1){iv=img02;}
-        if(i==2){iv=img11;}
-        if(i==3){iv=img12;}
-        if(i==4){iv=img03;}
-        if(i==5){iv=img13;}
-        if(i==6){iv=img00;}
-        if(i==7){iv=img10;}
+        if(i==0){iv= img01;}
+        if(i==1){iv= img02;}
+        if(i==2){iv= img11;}
+        if(i==3){iv= img12;}
+        if(i==4){iv= img03;}
+        if(i==5){iv= img13;}
+        if(i==6){iv= img00;}
+        if(i==7){iv= img10;}
         return iv;
     }
     
@@ -210,19 +298,28 @@ public class CuantosHayGameController implements Initializable {
                 String imagenElegida= imagenes.get(r);
                 ImageView iv=getIView(r);
                 setImage(imagenElegida,iv);
+                
+                
             }
 
     }
 
     }
     
-    class ImagenesEjercicio{
-        ArrayList<String> imagenesxEjercicio;
+    class Ejercicio{
+        int respuesta;
+        ArrayList<String> imagenes;
+        int intentos;
 
-        public ImagenesEjercicio(ArrayList<String> imagenesxEjercicio) {
-            this.imagenesxEjercicio = imagenesxEjercicio;
-        }      
+        public Ejercicio(int respuesta, ArrayList<String> imagenes, int intentos) {
+            this.respuesta = respuesta;
+            this.imagenes = imagenes;
+            this.intentos = intentos;
+        }
+
         
+
+ 
     }
-  
+    
 }
