02d9cae37efa12ccecb82d9333d843090e0aabd1
rgaibor17 <108085022+rgaibor17@users.noreply.github.com>
2022-08-25 07:11:53 -0500

    Update CitasController.java

--- src/main/java/espol/edu/ec/borradorproyectofx/CitasController.java
+++ src/main/java/espol/edu/ec/borradorproyectofx/CitasController.java
@@ -9,10 +9,12 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-
+import javafx.scene.layout.VBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
